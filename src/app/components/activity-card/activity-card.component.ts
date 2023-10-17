5fbc34b3e5322368d989b53d47eb3cfea940fe45
rechegoy <rebeca.echegoy@gmail.com>
2023-10-17 17:52:49 -0300

    Funcionalidad editar y eliminar

--- src/app/components/activity-card/activity-card.component.ts
+++ src/app/components/activity-card/activity-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-activity-card',
@@ -11,5 +11,16 @@ export class ActivityCardComponent {
   @Input('startDate') startDate: string = '';
   @Input('endDate') endDate: string = '';
   @Input('status') status: string = '';
+  @Input('activityId') activityId: number = 0;
 
+  @Output() eventEditarActividad: EventEmitter<number> = new EventEmitter<number>();
+  @Output() eventEliminarActividad: EventEmitter<number> = new EventEmitter<number>();
+
+  editarActividad(): void {
+    this.eventEditarActividad.emit(this.activityId);
+  }
+
+  eliminarActividad(): void {
+    this.eventEliminarActividad.emit(this.activityId);
+  }
 }
--- src/app/modules/itinerario/components/itinerario/itinerario.component.ts
+++ src/app/modules/itinerario/components/itinerario/itinerario.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, TemplateRef} from '@angular/core';
+import {Component, OnInit, TemplateRef, ChangeDetectorRef, ViewChild } from '@angular/core';
 import {ActividadesService} from "../../../../services/actividades.service";
 import {NgbOffcanvas} from "@ng-bootstrap/ng-bootstrap";
 import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
@@ -10,6 +10,7 @@ import {Actividad} from "../../../../interfaces/actividad";
   styleUrls: ['./itinerario.component.scss']
 })
 export class ItinerarioComponent implements OnInit{
+  @ViewChild('content') content: TemplateRef<any> | undefined;
   formNuevaActividad: FormGroup = new FormGroup({});
   actividades: Actividad[] = [];
   actividadesSinFecha: Actividad[] = [];
@@ -20,7 +21,8 @@ export class ItinerarioComponent implements OnInit{
   constructor(
     private actividadesService: ActividadesService,
     private offcanvasService: NgbOffcanvas,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private cd: ChangeDetectorRef
   ) {
     this.formNuevaActividad = this.fb.group({
       nombre: ['',[Validators.required]],
@@ -44,19 +46,53 @@ export class ItinerarioComponent implements OnInit{
     })
   }
 
+  agregarActividadConFecha(key: string, actividad: any): void {
+    if (!this.actividadesConFecha[key]) {
+      this.actividadesConFecha[key] = {};
+      this.actividadesConFecha[key]['fecha'] = key;
+      this.actividadesConFecha[key]['actividades'] = [];
+    }
+
+    this.actividadesConFecha[key]['actividades'].push(actividad);
+  }
+
+  getActividadPorIdActivity(activityId: number): Actividad | undefined {
+    let actividad: Actividad | undefined = this.actividadesSinFecha.find( ( actividad: Actividad )=>{
+      return actividad.activityId === activityId;
+    });
+
+    if(!actividad) {
+      Object.values(this.actividadesConFecha).forEach( ( value: any ) => {
+        if ( !actividad ) {
+          actividad = value.actividades.find( ( actividad: Actividad ) => {
+            return actividad.activityId === activityId;
+          });
+        };
+      });
+    };
+
+    return actividad;
+  }
+
+  editar(activityId: number): void {
+    const actividadAEditar = this.getActividadPorIdActivity(activityId);
+
+    this.formNuevaActividad.get('nombre')?.setValue(actividadAEditar?.title)
+    this.formNuevaActividad.get('fechaInicio')?.setValue(actividadAEditar?.startDate)
+    this.formNuevaActividad.get('fechaFinalizacion')?.setValue(actividadAEditar?.endDate)
+
+    if ( this.content ) {
+      this.openNuevaActividad( this.content )
+    }
+  }
+
   agruparPorFechas(): void {
    this.actividades.forEach((actividad: Actividad)=>{
      if (actividad.startDate) {
        const startDate = new Date( actividad.startDate );
        const dia_mes = startDate.getDate() + '-' + ( startDate.getMonth() + 1);
 
-       if (!this.actividadesConFecha[dia_mes]) {
-         this.actividadesConFecha[dia_mes] = {};
-         this.actividadesConFecha[dia_mes]['fecha'] = dia_mes;
-         this.actividadesConFecha[dia_mes]['actividades'] = [];
-       }
-
-       this.actividadesConFecha[dia_mes]['actividades'].push(actividad);
+       this.agregarActividadConFecha(dia_mes, actividad);
 
      } else {
        this.actividadesSinFecha.push(actividad);
@@ -74,5 +110,57 @@ export class ItinerarioComponent implements OnInit{
 
   get fActivity(): { [key: string]: AbstractControl } {
     return this.formNuevaActividad.controls;
+  }
+
+  agregarNuevaActividad(): void {
+    const nombre: string = this.fActivity['nombre'].value;
+    const fechaInicio: string = this.fActivity['fechaInicio'].value;
+    const fechaFinalizacion: string = this.fActivity['fechaFinalizacion'].value;
+
+   const maxActivityId = this.actividades.reduce((max, actividad) => Math.max(max, actividad.activityId), 0);
+
+  // Calcular el nuevo activityId
+  const nuevoActivityId = maxActivityId + 5;
+
+    const dataTempActividades: Actividad = {
+      'activityId':nuevoActivityId,
+      'title': nombre,
+      'type': 'ACTIVITY',
+      'startDate': fechaInicio,
+      'endDate': fechaFinalizacion,
+      'status': 'IN_PROGRESS'
+    }
+   
+   
+    const startDate = new Date( fechaInicio );
+    const dia_mes = startDate.getUTCDate() + '-' + ( startDate.getUTCMonth() + 1);
+
+    this.agregarActividadConFecha(dia_mes, dataTempActividades);
+
+    this.offcanvasService.dismiss();
+   
+  }
+
+  eliminarActividad(activityId: number): void {    
+    let indice: number | undefined = this.actividadesSinFecha.findIndex( ( actividad: Actividad )=>{
+      return actividad.activityId === activityId;
+    });
+
+    if(indice !== -1) {
+      this.actividadesSinFecha.splice(indice, 1);
+    } else {
+      Object.values(this.actividadesConFecha).forEach( ( value: any ) => {
+        if ( indice === -1 ) {
+          indice = value.actividades.findIndex( ( actividad: Actividad ) => {
+            return actividad.activityId === activityId;
+          });
+        };
+
+        if(indice !== -1) {
+          value.actividades.splice(indice, 1);
+          indice = -1;
+        } 
+      });
+    }
   }
 }
